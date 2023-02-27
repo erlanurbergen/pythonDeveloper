543f8d8a4f4e2e96ec059eebcb67d6b9dca50da2
erlankarabaliyev <erlanurbergen@gmail.com>
2023-02-27 14:37:40 +0600

    lesson 2, 3

---
+++ lesson2/main.py
@@ -0,0 +1,16 @@
+# lesson 2
+# console input. method input()
+first_number = 25
+first_number = 30
+
+# second_number = int(input("Insert number: "))
+# print(type(second_number))
+# print(second_number + 20)
+third_number = input("Insert third: ")
+print(third_number, "to", 20)
+print(third_number + " " + str(20))
+
+
+
+
+
---
+++ lesson3/main.py
@@ -0,0 +1,22 @@
+import math
+"""
+Модуль math – один из наиважнейших в Python.
+Этот модуль предоставляет обширный функционал для работы с числами.
+math.ceil(X) – округление до ближайшего большего числа.
+math.factorial(X) - факториал числа X.
+math.floor(X) - округление вниз.
+math.isfinite(X) - является ли X числом.
+math.trunc(X) - усекает значение X до целого.
+math.pow(X, Y) - XY.
+math.sqrt(X) - квадратный корень из X.
+math.pi - pi = 3,1415926...
+"""
+print(math.ceil(13.9)) # 13.4
+print(math.factorial(5))
+print(math.floor(13.9))
+print(math.isfinite(1))
+print(math.trunc(10.1))
+print(int(math.pow(6, 2)))
+print(math.sqrt(100))
+print(math.pi)
+print(10 // 3)
