aa7a680855b8cc2be3372f86f62c24ff2818a2f6
erlankarabaliyev <erlanurbergen@gmail.com>
2023-02-27 13:56:37 +0600

    Initial commit

---
+++ lesson1/main.py
@@ -0,0 +1,13 @@
+# lesson 1
+# a = 2
+# b = 2
+# c = a % b == 0 # bool (True False)
+# check = not a > b # a > b or a == b
+# # console output
+# print(check)
+a = 10
+b = "13.3"
+c = "1"
+x = int(c)
+print(a + float(b))
+print(type(x))
